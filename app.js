bb86eed35c23897c30f4dd0bb69786479884ee3e
Big Man <froxyopro@yahoo.fr>
2024-09-12 20:31:07 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from 'express'
+
+import { getComments, createComment } from './database.js';
+
+const app = express()
+app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.get('/comments', async (req, res) => {
+    const comments = await getComments()
+    res.send(comments)
+})
+
+app.post('/comments', async (req, res) => {
+    const { user, comment } = req.body
+    const result = await createComment(user, comment)
+    res.status(201).send(String(result))
+})
+
+app.listen(8080, () => {
+    console.log('Server is running on port 8080')
+})
---
+++ database.js
@@ -0,0 +1,22 @@
+import mysql from 'mysql2'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise()
+
+export async function getComments() {
+    const [rows] = await pool.query('select * from comments')
+    return rows
+}
+
+export async function createComment(user, comment) {
+    const [result] = await pool.query('insert into comments (user, comment) values (?, ?)', [user, comment])
+    return result.insertId
+}
+
